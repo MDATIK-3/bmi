aef4546a1f527658d5128027b27c5cef3d689da6
MD. ATIK <140510725+MDATIK-3@users.noreply.github.com>
2023-12-24 11:37:52 +0600

    Update javascript.js
    changed all data type 'var' to 'const' and 'let'

--- javascript.js
+++ javascript.js
@@ -1,36 +1,39 @@
 
-var form = document.getElementById('convert');
-var answer = document.getElementById('answer');
-var weightType = 'Weight'; // Initial weight type
-var b = document.getElementById('b');
-var m = document.getElementById('m');
+const form = document.getElementById('convert');
+const answer = document.getElementById('answer');
+
+const weightType = 'Weight'; // Initial weight type
+
+const b = document.getElementById('b');
+const m = document.getElementById('m');
+const weight = document.getElementById('weight');
 
 // Event listener for button clicks
 document.getElementById('b').addEventListener('click', switchToBMI);
 document.getElementById('m').addEventListener('click', switchToWeight);
 
-function switchToBMI(){
+function switchToBMI() {
 
     weightType = 'Weight';
     weight.innerHTML = 'Weight';
-    var kginput = document.getElementById('kg');
+   let kginput = document.getElementById('kg');
     kginput.setAttribute('placeholder', 'kg');
 }
-function switchToWeight(){
+function switchToWeight() {
     weightType = 'BMI';
-        weight.innerHTML = 'BMI';
-        var kginput = document.getElementById('kg');
-        kginput.setAttribute('placeholder', 'BMI');
+    weight.innerHTML = 'BMI';
+   let kginput = document.getElementById('kg');
+    kginput.setAttribute('placeholder', 'BMI');
 }
 
 
 document.addEventListener('keydown', function (event) {
-    var key = event.code;
-    var weight = document.getElementById('weight');
+    let key = event.code;
+ 
 
     if (key === 'KeyB') {
         switchToBMI();
-     
+
     } else if (key === 'KeyM') {
         switchToWeight();
     }
@@ -39,21 +42,25 @@ document.addEventListener('keydown', function (event) {
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    var ft = parseFloat(document.getElementById('ft').value) || 0;
-    var inch = parseFloat(document.getElementById('inch').value) || 0;
-    var kg = parseFloat(document.getElementById('kg').value) || 0;
-    var height = ((ft * 12 + inch) * 2.54) / 100;
+    let ft = parseFloat(document.getElementById('ft').value) || 0;
+    let inch = parseFloat(document.getElementById('inch').value) || 0;
+    let kg = parseFloat(document.getElementById('kg').value) || 0;
+    let height = ((ft * 12 + inch) * 2.54) / 100;
     height = height * height;
 
     if (weightType === 'BMI') {
-        var ans = kg * height;
+        let ans = kg * height;
         if (ans) {
             answer.innerHTML = 'The weight is: ' + ans.toFixed(2);
         } else {
             answer.innerHTML = 'Please enter values';
         }
-    } else if (weightType === 'Weight') {
-        var bmi = kg / height;
+    }
+    
+    
+    else if (weightType === 'Weight') {
+       
+        let bmi = kg / height;
         if (bmi) {
             answer.innerHTML = 'The BMI is: ' + bmi.toFixed(2);
         } else {
